a033f53faacdb8f1f6749025d339d3c71a072aa2
amarriner <amarriner@amarriner.com>
2015-10-09 15:27:53 -0400

    Added Less and fiddled with layout

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            default: {
+                options: {},
+                files: {
+                    "public/css/app.css": "less/app.less"
+                }
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+}
